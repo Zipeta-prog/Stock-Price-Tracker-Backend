f6cc2a2f3d295eb40836f2bbb9e08842a27837cb
Zipeta-prog <zipeta81@gmail.com>
2024-08-23 17:32:58 +0300

    security configuration

--- src/main/java/com/example/stockprice/config/SecurityConfig.java
+++ src/main/java/com/example/stockprice/config/SecurityConfig.java
@@ -2,28 +2,33 @@ package com.example.stockprice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @SuppressWarnings({ "deprecation", "removal" })
+	protected void configure(HttpSecurity http) throws Exception {
         http
-            .csrf().disable()
-            .authorizeRequests()
-                .antMatchers("/api/stocks/**").authenticated()  // Require authentication for stock endpoints
-                .anyRequest().permitAll()
-            .and()
-            .httpBasic();  // Use basic authentication for simplicity
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(requests -> requests
+                        .requestMatchers("/api/stocks/**").authenticated()
+                        .anyRequest().permitAll())
+                .httpBasic(withDefaults());
     }
 
-    @Bean
+    private Customizer<HttpBasicConfigurer<HttpSecurity>> withDefaults() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
